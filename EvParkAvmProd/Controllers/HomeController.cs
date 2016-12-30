d91c05e6a243ecd7b9ec28895fcba2f47e02ccf9
Z003KJVZ <a>
2016-12-30 16:20:31 +0300

    Add project files.

---
+++ EvParkAvmProd/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EvParkAvmProd.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public ActionResult About()
+        {
+            return View("About");
+        }
+
+        public ActionResult Contact()
+        {
+            return View("Contact");
+        }
+    }
+}
